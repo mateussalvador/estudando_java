41f4024dd5511cd542983bd2b9cb240b55c3606b
mateussalvador <mateuslimasalvador@gmail.com>
2025-01-23 15:47:56 -0300

    Primeiro programa em Java: Hello World

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module estudando_java {
+}
---
+++ src/salvador/mateus/introducao/A_HelloWorld.java
@@ -0,0 +1,7 @@
+package salvador.mateus.introducao;
+
+public class A_HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
